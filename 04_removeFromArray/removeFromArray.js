452f8dbf11151dfbcb06642d1f2ce8d9c9473041
James <jamesflorez11@gmail.com>
2021-12-30 16:59:13 -0800

    started 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...val) {
+    let ans=arr;
+    console.log(val.length) // why isn't val capturing all input parameters??
+    for(let i=0;i<arr.length;i++) {
+        for(let j=0;j<val.length;j++) {
+            if(arr[i]===val[j]) {
+                let removed=arr.splice(i,1);
+            }
+        }
+    }
+    return ans;
 };
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromsArray;
