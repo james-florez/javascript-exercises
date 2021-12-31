09e33c1d49b6e6ee8c0d105f1a2fc31b0ebc0d36
James <jamesflorez11@gmail.com>
2021-12-30 16:07:38 -0800

    passed 1st test repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    
 
+    if(num<0)
+        return 'ERROR';
+    else if (num===0)
+        return '';
+    else {
+        let ans="";
+        for (let i=0;i<num;i++) {
+            ans=ans+string;
+        }
+        return ans;
+    }
 };
 
 // Do not edit below this line
