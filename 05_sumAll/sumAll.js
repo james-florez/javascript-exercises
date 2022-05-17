686776110d8aee265c652351c2f96b4bb9890a16
James <jamesflorez11@gmail.com>
2022-05-17 16:33:42 -0400

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    // Validate input
+    if (! Number.isInteger(val1) || ! Number.isInteger(val2) || val1 < 0 || val2 < 0) {
+        return "ERROR";
+    }
 
+    // Order input
+    let minVal = Math.min(val1, val2)
+    let maxVal = Math.max(val1, val2)
+    
+    let ans = 0;
+    for(let i = minVal; i <= maxVal; i++) {
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
