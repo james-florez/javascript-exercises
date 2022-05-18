7ab75cc4a03a00910efcf57507330ffa17f915c9
James <jamesflorez11@gmail.com>
2022-05-18 11:30:09 -0400

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+    // C = (F - 32) * 5/9
+    let tempC = (tempF - 32) * 5 / 9
+    return +tempC.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+    // F = C * 9/5 + 32
+    let tempF = tempC * 9 / 5 + 32
+    return +tempF.toFixed(1);
 };
 
 // Do not edit below this line
