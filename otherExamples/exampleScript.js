12367ae9b9f04f9d0bd0037a931156efe19a13d3
James <jamesflorez11@gmail.com>
2022-05-19 16:37:56 -0400

    worked on JS DOM examples

--- otherExamples/exampleScript.js
+++ otherExamples/exampleScript.js
@@ -35,3 +35,31 @@ p.textContent = 'ME TOO!';
 div.appendChild(p);
 
 container.appendChild(div);
+
+// Button
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("JS Hello World");
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  alert("Hello World");
+});
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', function (e) {
+    console.log(e);
+    e.target.style.background = 'blue';
+  });
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
